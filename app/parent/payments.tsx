5b4ab4e91573be6e42d4afc667506d927b4ba1d7
NyLaurent <laulanyumbayire@gmail.com>
2025-06-05 22:05:12 -0500

    feat: buddi ranking screens

--- app/parent/payments.tsx
+++ app/parent/payments.tsx
@@ -1,31 +1,56 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import CallUpReviewCard from '@/components/parent/CallUpReviewCard'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import PageHeader from "@/components/commons/PageHeader";
+import CallUpReviewCard from "@/components/parent/CallUpReviewCard";
+import NoBuddiesReceivedCard from "@/components/parent/NoBuddiesReceivedCard";
+import React from "react";
+import { Platform, ScrollView, Text, View } from "react-native";
 
 const payments = () => {
   return (
-    <SafeAreaView className='flex-1 bg-white'>
-          <Text>payments</Text>
-    
+    <>
+      <View
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: Platform.OS === "android" ? 32 : 0,
+          backgroundColor: "white",
+          zIndex: 10,
+        }}
+      />
+      <ScrollView
+        className="flex-1 bg-white"
+        contentContainerStyle={{
+          paddingTop: Platform.OS === "android" ? 32 : 0,
+          minHeight: "100%",
+        }}
+      >
+        <PageHeader title="My Buddi" />
+        <Text className="text-lg font-comfortaa-bold px-2">
+          Call Up Preview
+        </Text>
+        <View className="px-2">
+          <CallUpReviewCard
+            name="Brian Ford"
+            email="brianford@lok.com"
+            school="School, Name"
+            requestedAgo="2 Days ago"
+            description="Fill in the details below to invite Buddis to apply."
+            schoolName="School Name"
+            home="Senen"
+            assignedKids={[{ name: "Bryan Smith" }, { name: "Bryan Smith" }]}
+            onRemove={() => {
+              /* handle remove */
+            }}
+            onApplicants={() => {
+              /* handle applicants */
+            }}
+          />
+          <NoBuddiesReceivedCard />
+        </View>
+      </ScrollView>
+    </>
+  );
+};
 
-<CallUpReviewCard
-  name="Brian Ford"
-  email="brianford@lok.com"
-  school="School, Name"
-  requestedAgo="2 Days ago"
-  description="Fill in the details below to invite Buddis to apply."
-  schoolName="School Name"
-  home="Senen"
-  assignedKids={[
-    { name: "Bryan Smith" },
-    { name: "Bryan Smith" }
-  ]}
-  onRemove={() => { /* handle remove */ }}
-  onApplicants={() => { /* handle applicants */ }}
-/>
-    </SafeAreaView>
-  )
-}
-
-export default payments
+export default payments;
